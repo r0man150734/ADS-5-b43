eaa0b2f4b1520fbc12f093f6d0bb01fe8d592fbf
r0man150734 <124446971+r0man150734@users.noreply.github.com>
2025-06-07 16:13:44 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -8,55 +8,56 @@
 template<typename T, int size>
 class TStack {
  private:
-  std::array<T, size> data;
-  int topIndex;
+    std::array<T, size> data;
+    int topIndex;
 
  public:
-  TStack() : topIndex(-1) {}
+    TStack() : topIndex(-1) {}
 
-  void push(const T& value) {
-    if (isFull()) {
-      throw std::runtime_error("Stack overflow");
+    void push(const T& value) {
+        if (isFull()) {
+            throw std::runtime_error("Stack overflow");
+        }
+        data[++topIndex] = value;
     }
-    data[++topIndex] = value;
-  }
 
-  void pop() {
-    if (isEmpty()) {
-      throw std::runtime_error("Stack underflow");
+    T pop() {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack underflow");
+        }
+        return data[topIndex--];
     }
-    --topIndex;
-  }
 
-  T& top() {
-    if (isEmpty()) {
-      throw std::runtime_error("Stack is empty");
+
+    T& top() {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack is empty");
+        }
+        return data[topIndex];
     }
-    return data[topIndex];
-  }
 
-  const T& top() const {
-    if (isEmpty()) {
-      throw std::runtime_error("Stack is empty");
+    const T& top() const {
+        if (isEmpty()) {
+            throw std::runtime_error("Stack is empty");
+        }
+        return data[topIndex];
     }
-    return data[topIndex];
-  }
 
-  bool isEmpty() const {
-    return topIndex == -1;
-  }
+    bool isEmpty() const {
+        return topIndex == -1;
+    }
 
-  bool isFull() const {
-    return topIndex == size - 1;
-  }
+    bool isFull() const {
+        return topIndex == size - 1;
+    }
 
-  int count() const {
-    return topIndex + 1;
-  }
+    int count() const {
+        return topIndex + 1;
+    }
 
-  void clear() {
-    topIndex = -1;
-  }
+    void clear() {
+        topIndex = -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
